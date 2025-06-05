9ae4644deb39c07e66c7e67b9165a7ef29644183
snayak <nayak.shravan07@gmail.com>
2025-06-05 22:12:53 +0530

    Added RabbitMQ project

---
+++ RabbitMq.Messaging/RabbitMq.Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(
+    queue: "message",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+for(int i= 1;i<=10; i++)
+{
+    var message = "Message "+i+" "+DateTime.UtcNow;
+    var body=Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+        exchange: string.Empty,
+        routingKey: "message",
+        mandatory: true,
+        basicProperties: new BasicProperties { Persistent = true },
+        body: body);
+
+    Console.WriteLine("Sent " + message);
+
+    await Task.Delay(2000);
+}
